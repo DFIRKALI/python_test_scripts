70e134d4da0cf2d60aba896a023e4b6cad2d15aa
DFIRKALI <elliotg4@gmail.com>
2018-07-23 21:29:46 -0400

    First Test

---
+++ new_script.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import os
+
+#this is a simple that displays OS informaion and parses some json from an API
+
+r.requests = (https://jsonplaceholder.typicode.com/users/, verfy=False)
+
+print(r.text)
+
+print(os.system('ifconfig'))
+print(os.system('df -h'))
+
+
